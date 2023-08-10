55014f7dad4ebc916cb64bdd82a3acb989a039f6
Chibueze Amadi <123497071+Elmagn1f1c0@users.noreply.github.com>
2023-08-10 17:21:13 +0100

    My first commit

---
+++ youtube-clone/src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDALGqW5ZcfMxA4rjf3-Q_Ptf64C1Xcubc",
+    authDomain: "clone-5ee4a.firebaseapp.com",
+    databaseURL: "https://clone-5ee4a.firebaseio.com",
+    projectId: "clone-5ee4a",
+    storageBucket: "clone-5ee4a.appspot.com",
+    messagingSenderId: "134655923155",
+    appId: "1:134655923155:web:c3b6f157d4a4dfb958d526",
+    measurementId: "G-9X4SNCYQS3"
+  };
