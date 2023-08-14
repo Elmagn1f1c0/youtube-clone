fedc1a40c27e668539256b9ad51354cdd35a0194
Chibueze Amadi <123497071+Elmagn1f1c0@users.noreply.github.com>
2023-08-14 15:39:59 +0100

    Recommended Video

--- youtube-clone/src/App.js
+++ youtube-clone/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
 import RecommendedVideos from './RecommendedVideos';
+import { BrowserRouter  as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
--- youtube-clone/src/SidebarRow.js
+++ youtube-clone/src/SidebarRow.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './SidebarRow.css';
 
-function SidebarRow({ Icon, title }) {
+function SidebarRow({ selected, Icon, title }) {
   return (
-    <div className="sidebarRow">
+    <div className={`sidebarRow ${selected && "selected"}`}>
     <Icon className="sidebarRow__icon"/>
         <h2 className="sidebarRow__title">{title}</h2>
     </div>
   );
 }
 
-export default SidebarRow;
+export default SidebarRow; 
